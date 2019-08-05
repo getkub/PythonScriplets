ce44170210f43c9e0ce6d4580aeeabcc2ea2576a
getkub <getkub@users.noreply.github.com>
2019-08-05 16:00:15 +0100

    Create mp3Convert.py

---
+++ youtube-dl/mp3Convert.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+# Pass youtube URL and download to mp3
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+video_name = raw_input('Enter youtube video name: ')
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([video_name])
